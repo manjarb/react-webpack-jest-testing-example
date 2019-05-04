fe6c56fd1167efa198a69caf3d41528ea84dd2be
Varis Darasirikul <varis.dara@gmail.com>
2019-05-05 03:52:24 +0800

    Add client files

---
+++ client/src/screens/__tests__/editor.todo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Editor from '../editor.todo'
+
+test('calls onSubmit with the username and password when submitted', () => {
+  const container = document.createElement('div')
+  ReactDom.render(<Editor/>, container)
+  const form = container.querySelector('form')
+  const {title, content, tags} = form.elements
+  console.log(title, 'title 3peus4u eeiueeueeuuueeeueeee eue')
+  console.log(content, 'content')
+  // Arrange
+  // create a fake user, post, history, and api
+  //
+  // use ReactDOM.render() to render the editor to a div
+  //
+  // fill out form elements with your fake post
+  //
+  // Act
+  // submit form
+  //
+  // wait for promise to settle
+  //
+  // Assert
+  // ensure the create function was called with the right data
+})
+
+// TODO later...
+test('snapshot', () => {})
