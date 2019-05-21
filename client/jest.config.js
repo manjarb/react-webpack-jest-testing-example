27d263203361590b84e68faeafe78c2aa664612d
manjarb <varis.dara@gmail.com>
2019-05-21 18:02:45 +0800

    Update jest.config.js

--- client/jest.config.js
+++ client/jest.config.js
@@ -15,12 +15,15 @@ module.exports = {
     moduleFileExtensions: ['js', 'json', 'jsx'],
     modulePaths: ['<rootDir>/src', '<rootDir>/test'],
     moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
         '\\.svg$': '<rootDir>/test/svg-file-mock.js',
     },
 
     // The paths to modules that run some code to configure or set up the testing environment before each test
     // setupFiles: ['<rootDir>/enzyme.config.js'],
-    setupFilesAfterEnv: ['./test/setup-test-framework.js'],
+    // setupFilesAfterEnv: ['./test/setup-test-framework.js'],
+    setupFilesAfterEnv: ['./node_modules/jest-enzyme/lib/index.js', '<rootDir>/jest.init.js'],
 
     // The test environment that will be used for testing
     testEnvironment: 'jsdom',
